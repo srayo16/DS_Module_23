3ba6b84aa1689fa04bae18da1306b9fd6ee3a809
srayo16 <srayosikder@gmail.com>
2023-07-19 00:09:48 +0600

    pq

---
+++ priority_queue.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    priority_queue<int> pq;
+
+    while (true)
+    {
+        int c;
+        cin >> c;
+
+        if (c == 0)
+        {
+            int v;
+            cin >> v;
+            pq.push(v); // O(logN)
+        }
+        else if (c == 1)
+        {
+            pq.pop(); // O(logN)
+        }
+        else if (c == 2)
+        {
+            cout << pq.top() << endl; // O(1);
+        }
+        else if (c == 3)
+        {
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    return 0;
+}
